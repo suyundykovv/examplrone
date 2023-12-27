d66b68cec85bca1893156dc75d79b588d1c85284
suyundykovv <230639@astanait.edu.kz>
2023-12-27 22:54:47 +0600

    finished shape

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -8,7 +8,14 @@ public class MyApplication {
         ArrayList<Point> points = readCoordinatesFromFile("Coordinates");
 
     }
-    Shape shape = new shape();
+    Shape shape = new Shape();
 
+    double perimeter = shape.calculatePerimeter();
+    double longestSide = shape.findLongestSide();
+    double averageSide = shape.findAverageSide();
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Longest Side: " + longestSide);
+        System.out.println("Average Side: " + averageSide);
 
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
 public class Shape {
+    private ArrayList<Point> points;
+
+    public Shape(ArrayList<Point> points) {
+        this.points = points;
+}
+    public double calculatePerimeter() {
+        double perimeter = 0;
+        int size = points.size();
+
+        for (int i = 0; i < size; i++) {
+            Point currentPoint = points.get(i);
+            Point nextPoint = points.get((i + 1) % size);
+            perimeter += currentPoint.distance(nextPoint);
+        }
+
+        return perimeter;
+    }
+    public double findLongestSide() {
+        double longestSide = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            Point currentPoint = points.get(i);
+            Point nextPoint = points.get((i + 1) % points.size()); // Circular, connects last to first point
+
+            double currentSide = currentPoint.distance(nextPoint);
+            if (currentSide > longestSide) {
+                longestSide = currentSide;
+            }
+        }
+
+        return longestSide;
+    }
+
+    public double findAverageSide() {
+        double totalSideLength = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            Point currentPoint = points.get(i);
+            Point nextPoint = points.get((i + 1) % points.size()); // Circular, connects last to first point
+
+            totalSideLength += currentPoint.distance(nextPoint);
+        }
+
+        return totalSideLength / points.size();
+    }
 }
